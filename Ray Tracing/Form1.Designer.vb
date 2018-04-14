d2c8a3e85d255fd68bab2be014d3a0c8df909324
Predrika Br Ginting <ichapredrikagm@gmail.com>
2018-04-14 19:49:14 +0700

    -------------

--- Ray Tracing/Form1.Designer.vb
+++ Ray Tracing/Form1.Designer.vb
@@ -24,83 +24,41 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.refreshBtn = New System.Windows.Forms.Button()
-        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
-        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
-        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(17, 16)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(644, 502)
+        Me.PictureBox1.Size = New System.Drawing.Size(483, 408)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'refreshBtn
         '
-        Me.refreshBtn.Location = New System.Drawing.Point(724, 275)
-        Me.refreshBtn.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.refreshBtn.Location = New System.Drawing.Point(551, 59)
         Me.refreshBtn.Name = "refreshBtn"
-        Me.refreshBtn.Size = New System.Drawing.Size(100, 28)
+        Me.refreshBtn.Size = New System.Drawing.Size(75, 23)
         Me.refreshBtn.TabIndex = 1
         Me.refreshBtn.Text = "Refresh"
         Me.refreshBtn.UseVisualStyleBackColor = True
         '
-        'TrackBar1
-        '
-        Me.TrackBar1.Location = New System.Drawing.Point(668, 16)
-        Me.TrackBar1.Maximum = 300
-        Me.TrackBar1.Name = "TrackBar1"
-        Me.TrackBar1.Size = New System.Drawing.Size(345, 56)
-        Me.TrackBar1.TabIndex = 2
-        '
-        'TrackBar2
-        '
-        Me.TrackBar2.Location = New System.Drawing.Point(668, 65)
-        Me.TrackBar2.Maximum = 300
-        Me.TrackBar2.Name = "TrackBar2"
-        Me.TrackBar2.Size = New System.Drawing.Size(345, 56)
-        Me.TrackBar2.TabIndex = 3
-        '
-        'TrackBar3
-        '
-        Me.TrackBar3.Location = New System.Drawing.Point(668, 117)
-        Me.TrackBar3.Maximum = 300
-        Me.TrackBar3.Name = "TrackBar3"
-        Me.TrackBar3.Size = New System.Drawing.Size(345, 56)
-        Me.TrackBar3.TabIndex = 4
-        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1025, 533)
-        Me.Controls.Add(Me.TrackBar3)
-        Me.Controls.Add(Me.TrackBar2)
-        Me.Controls.Add(Me.TrackBar1)
+        Me.ClientSize = New System.Drawing.Size(769, 433)
         Me.Controls.Add(Me.refreshBtn)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Ray Tracing"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents refreshBtn As Button
-    Friend WithEvents TrackBar1 As TrackBar
-    Friend WithEvents TrackBar2 As TrackBar
-    Friend WithEvents TrackBar3 As TrackBar
 End Class
